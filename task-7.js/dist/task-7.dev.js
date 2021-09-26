b09e8dd143fa5bb97522604da211950a9dff4df8
Ilona Shevchuk <ilona.shvk@icloud.com>
2021-09-26 17:59:32 +0300

    task-7.JS
    внесла правки в шлях.

--- task-7.js/dist/task-7.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var controlFont = document.getElementById('font-size-control');
-var text = document.getElementById('text');
-
-controlFont.oninput = function () {
-  text.style.fontSize = controlFont.value + 'px';
-};
